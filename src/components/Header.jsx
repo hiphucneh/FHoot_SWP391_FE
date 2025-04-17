fd78e700e2291ea12d50dfc7011bc6a267243618
hiphucneh <anhdung200505@gmail.com>
2025-04-17 19:01:37 +0700

    Sửa header đẹp hơn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,60 @@
-import React from 'react';
-import './Header.css'; // Import CSS cho header
+import React from "react";
+import "./Header.css"; // Import CSS cho header
+import { RiAccountCircleFill } from "react-icons/ri";
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <a href="#" className="logo">
-              <i className="fa-brands fa-pied-piper-alt"></i> {/* Logo */}
-            </a>
-          </li>
-          <li><a href="#" className="link">Home</a></li>
-          <li><a href="#" className="link">About</a></li>
-          <li><a href="#" className="link">Skills</a></li>
-          <li><a href="#" className="link">Projects</a></li>
-          <li>
-            <a href="#" className="link" id="hire-me">
-              <i className="fa-regular fa-envelope"></i> Hire me
-            </a>
-          </li>
-        </ul>
+    <header class="header" id="header">
+      <nav class="nav container_head">
+        <a href="#" class="nav__logo">
+          Logo
+        </a>
+
+        <div class="nav__menu" id="nav-menu">
+          <ul class="nav__list">
+            <li class="nav__item">
+              <a href="#" class="nav__link">
+                Home
+              </a>
+            </li>
+
+            <li class="nav__item">
+              <a href="#" class="nav__link">
+                About Us
+              </a>
+            </li>
+
+            <li class="nav__item">
+              <a href="#" class="nav__link">
+                Services
+              </a>
+            </li>
+
+            <li class="nav__item">
+              <a href="#" class="nav__link">
+                Featured
+              </a>
+            </li>
+
+            <li class="nav__item">
+              <a href="#" class="nav__link">
+                Contact Me
+              </a>
+            </li>
+
+            <li class="nav__item">
+              <a href="#" class="nav__link">
+                Log In
+              </a>
+            </li>
+
+            <li>
+              <a href="#" className="link" id="sign-up">
+                <i className="fa-regular fa-envelope"></i> Sign Up
+              </a>
+            </li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
