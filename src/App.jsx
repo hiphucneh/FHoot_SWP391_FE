ba95b7b97da9c739efa9040b6a536114118aa67c
NamHungBBB1 <163675302+NamHungBBB1@users.noreply.github.com>
2025-04-19 20:47:44 +0700

    delete

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
-import './components/styles.css'
-import 'remixicon/fonts/remixicon.css'
-import Header from './components/Header'
+import React from "react";
+import Login from "./components/Login";
+import WaitRoomScreen from "./screens/WaitRoomScreen.jsx";
+import QuestionScreen from "./screens/QuestionScreen";
+import CreateKahoot from "./screens/CreateKahootScreen";
+import CreateQuestion from "./screens/CreateQuestionScreen";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <>
+    <div className="App">
       <Header />
-    </>
-  )
+      <Router>
+        <Routes>
+          <Route path="/createK" element={<CreateKahoot />} />
+          <Route path="/createQ" element={<CreateQuestion />} />
+          <Route path="/waitRoom" element={<WaitRoomScreen />} />
+          <Route path="/question" element={<QuestionScreen />} />
+          <Route path="/login" element={<Login />} />
+
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App
