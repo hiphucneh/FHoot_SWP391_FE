1a0d94ec3ba7b9e1a4a88a040238d6a500d524fa
NamHungBBB1 <163675302+NamHungBBB1@users.noreply.github.com>
2025-04-20 19:45:50 +0700

    clean CreateK,create LeaderBoard

---
+++ src/screens/LeaderBoardScreen.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const LeaderBoardScreen = () => {
+    const testData = [
+        { id: 1, name: 'Player 1', score: 1500 },
+        { id: 2, name: 'Player 2', score: 1400 },
+        { id: 3, name: 'Player 3', score: 1300 },
+        { id: 4, name: 'Player 4', score: 1200 },
+        { id: 5, name: 'Player 5', score: 1100 },
+    ];
+
+    return (
+
+        <div>Hello</div>
+    );
+};
+
+
+
+
+export default LeaderBoardScreen;
