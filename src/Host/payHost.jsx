ffa971e4e39cc3c0cd875de1ae4cda5ee8e939b1
PhucPhuc1111 <133560614+PhucPhuc1111@users.noreply.github.com>
2025-04-30 21:40:48 +0700

    Update payHost.jsx

--- src/Host/payHost.jsx
+++ src/Host/payHost.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import './PayHost.css';
+import './payHost.css';
 import PayPopup from './PayPopup';
 
 function PayHost() {
